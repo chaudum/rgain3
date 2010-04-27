e7a2ef9ca4c9d8e1117d19f60fe97ef07144b1d0
Felix Krull <f_krull@gmx.de>
2010-04-27 23:33:24 +0200

    Changed license stuff to GPLv2 or later.
    Problem is, Mutagen is licensed under GPLv2 exclusively. However, GPLv2 is
    incompatible with GPLv3, so we can?t use v3(-or-later); have to use
    v2(-or-later).

--- rgain/rgcalc.py
+++ rgain/rgcalc.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # 
-# Copyright (c) 2009 Felix Krull <f_krull@gmx.de>
+# Copyright (c) 2009, 2010 Felix Krull <f_krull@gmx.de>
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
+# the Free Software Foundation; either version 2, or (at your option)
 # any later version.
 # 
 # This program is distributed in the hope that it will be useful,
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright (c) 2009 Felix Krull <f_krull@gmx.de>
 
 from distutils.core import setup
 
