826d8c9c941a77ce1a38fa04824f69c4e045412e
Felix Krull <f_krull@gmx.de>
2013-05-04 17:02:55 +0200

    Disable manpage generation if python-docutils is not installed.

--- setup.py
+++ setup.py
@@ -1,46 +1,60 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 from contextlib import closing
 import os
+import sys
 from distutils.core import Command, Distribution, setup
 from distutils.command.build import build
 from distutils.errors import DistutilsOptionError
-import docutils.core
 
-class ManpagesDistribution(Distribution):
-    def __init__(self, attrs=None):
-        self.rst_manpages = None
-        Distribution.__init__(self, attrs)
+try:
+    import docutils.core
 
-class build_manpages(Command):
-    description = "Generate man pages."
-    user_options = [
-        ("outputdir=", "b", "output directory for man pages"),
-    ]
-    
-    def initialize_options(self):
-        self.rst_manpages = None
-        self.outputdir = None
-    
-    def finalize_options(self):
-        if not self.outputdir:
-            self.outputdir = os.path.join("build", "man")
-        self.rst_manpages = self.distribution.rst_manpages
-    
-    def run(self):
-        if not self.rst_manpages:
-            return
-        if not os.path.exists(self.outputdir):
-            os.makedirs(self.outputdir, mode=0755)
-        for infile, outfile in self.rst_manpages:
-            print "Converting %s to %s ..." % (infile, outfile),
-            docutils.core.publish_file(source_path=infile,
-                    destination_path=os.path.join(self.outputdir, outfile),
-                    writer_name="manpage")
-            print "ok"
+    class ManpagesDistribution(Distribution):
+        def __init__(self, attrs=None):
+            self.rst_manpages = None
+            Distribution.__init__(self, attrs)
 
-build.sub_commands.append(("build_manpages", None))
+    class build_manpages(Command):
+        description = "Generate man pages."
+        user_options = [
+            ("outputdir=", "b", "output directory for man pages"),
+        ]
+
+        def initialize_options(self):
+            self.rst_manpages = None
+            self.outputdir = None
+        
+        def finalize_options(self):
+            if not self.outputdir:
+                self.outputdir = os.path.join("build", "man")
+            self.rst_manpages = self.distribution.rst_manpages
+
+        def run(self):
+            if not self.rst_manpages:
+                return
+            if not os.path.exists(self.outputdir):
+                os.makedirs(self.outputdir, mode=0755)
+            for infile, outfile in self.rst_manpages:
+                print "Converting %s to %s ..." % (infile, outfile),
+                docutils.core.publish_file(source_path=infile,
+                        destination_path=os.path.join(self.outputdir, outfile),
+                        writer_name="manpage")
+                print "ok"
+
+    build.sub_commands.append(("build_manpages", None))
+    manpages_args = {
+        "rst_manpages": [
+            ("man/replaygain.rst", "replaygain.1"),
+            ("man/collectiongain.rst", "collectiongain.1"),
+        ],
+        "cmdclass": {"build_manpages": build_manpages},
+        "distclass": ManpagesDistribution,
+    }
+except ImportError:
+    print >> sys.stderr, ("You do not have docutils, the manpages won't be "
+                          "generated.")
+    manpages_args = {}
 
 setup(
     name="rgain",
@@ -70,17 +84,10 @@ named cousins, most prominently ``vorbisgain`` and ``mp3gain`` -- only that it
 works for all those supported formats alike. ``collectiongain`` on the other
 hand is a kind of fire-and-forget tool for big amounts of music files.
 """,
-    
+
     packages=["rgain", "rgain.script"],
     scripts=["scripts/replaygain", "scripts/collectiongain"],
-    rst_manpages=[
-        ("man/replaygain.rst", "replaygain.1"),
-        ("man/collectiongain.rst", "collectiongain.1"),
-    ],
-    
     requires=["pygobject", "mutagen"],
-    
-    cmdclass={"build_manpages": build_manpages},
-    distclass=ManpagesDistribution
-)
 
+    **manpages_args
+)
