8bf7e72de0b9affe62eeaa3397d2ae3c738dffac
Felix Krull <f_krull@gmx.de>
2014-05-23 17:00:25 +0200

    Clean up rgio.py formatting.

--- rgain/rgio.py
+++ rgain/rgio.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright (c) 2009-2014 Felix Krull <f_krull@gmx.de>
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2, or (at your option)
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -16,7 +16,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-from itertools import chain, combinations
 import os.path
 import warnings
 
@@ -42,12 +41,14 @@ def parse_db(string):
         db = None
     return db
 
+
 def parse_peak(string):
     try:
         peak = float(string.strip())
     except ValueError:
         peak = None
     return peak
+
 
 def almost_equal(a, b, epsilon):
     if a is None and b is None:
@@ -64,6 +65,7 @@ class BaseTagReaderWriter(object):
 
     def write_gain(self, filename, track_gain, album_gain):
         raise NotImplementedError
+
 
 # class to read and write ReplayGain data from/to simple tags. The default tags
 # match the rg.org specification for Ogg (at least Vorbis), Flac and WavPack
@@ -144,6 +146,7 @@ class SimpleTagReaderWriter(BaseTagReaderWriter):
     def _dump_ref_level(self, ref_level):
         return u"%i dB" % ref_level
 
+
 # MP4 support
 class MP4TagReaderWriter(SimpleTagReaderWriter):
     FORMAT = "----:com.apple.iTunes:replaygain_%s_%s"
@@ -163,7 +166,8 @@ class MP4TagReaderWriter(SimpleTagReaderWriter):
         return SimpleTagReaderWriter._dump_peak(self, peak).encode("ascii")
 
     def _dump_ref_level(self, ref_level):
-        return SimpleTagReaderWriter._dump_ref_level(self, ref_level).encode("ascii")
+        return SimpleTagReaderWriter._dump_ref_level(
+            self, ref_level).encode("ascii")
 
 
 # MP3 support base class
@@ -176,7 +180,7 @@ class MP3TagReaderWriter(SimpleTagReaderWriter):
         u"replaygain_reference_loudness",
         u"QuodLibet::replaygain_reference_loudness",
     ]
-    
+
     class _ReplaygainEasyID3(EasyID3):
         pass
 
@@ -202,7 +206,8 @@ class MP3rgorgTagReaderWriter(MP3TagReaderWriter):
 RVA2_GAIN_MIN = -64
 RVA2_GAIN_MAX = float(64 * 512 - 1) / 512.0
 RVA2_PEAK_MIN = 0
-RVA2_PEAK_MAX = float(2**16 - 1) / float(2**15)
+RVA2_PEAK_MAX = float(2 ** 16 - 1) / float(2 ** 15)
+
 
 def clamp(v, min, max):
     clamped = False
@@ -214,19 +219,24 @@ def clamp(v, min, max):
         clamped = True
     return v, clamped
 
+
 def clamp_rva2_gain(v):
     v, clamped = clamp(v, RVA2_GAIN_MIN, RVA2_GAIN_MAX)
     if clamped:
-        warnings.warn("gain value was out of bounds for RVA2 frame and was clamped to %.2f" % v)
+        warnings.warn("gain value was out of bounds for RVA2 frame and was "
+                      "clamped to %.2f" % v)
     return v
+
 
 # I'm not sure if this situation could even reasonably happen, but
 # can't hurt to check, right? Right!?
 def clamp_rva2_peak(v):
     v, clamped = clamp(v, RVA2_PEAK_MIN, RVA2_PEAK_MAX)
     if clamped:
-        warnings.warn("peak value was out of bounds for RVA2 frame and was clamped to %.5f" % v)
+        warnings.warn("peak value was out of bounds for RVA2 frame and was "
+                      "clamped to %.5f" % v)
     return v
+
 
 def clamp_gain_data(gain_data):
     if gain_data is None:
@@ -270,20 +280,20 @@ class MP3DefaultTagReaderWriter(BaseTagReaderWriter):
         self.rva2 = rva2_readerwriter
 
     def read_gain(self, filename):
-        rgorg = self.rgorg.read_gain(filename)
-        rva2 = self.rva2.read_gain(filename)
-        # We want to ensure we have all bits of data so if we only have one format,
-        # we say we have none to enforce recalculation.
-        if rgorg[0] is None or rva2[0] is None:
+        rgorg_track_gain, rgorg_album_gain = self.rgorg.read_gain(filename)
+        rva2_track_gain, rva2_album_gain = self.rva2.read_gain(filename)
+        # We want to ensure we have all bits of data so if we only have one
+        # format, we say we have none to enforce recalculation.
+        if rgorg_track_gain is None or rva2_track_gain is None:
             # ensure that track gain exists for both
             return (None, None)
-        if (not gaindata_almost_equal(rgorg[0], rva2[0]) or
-            not gaindata_almost_equal(rgorg[1], rva2[1])):
+        if (not gaindata_almost_equal(rgorg_track_gain, rva2_track_gain) or
+                not gaindata_almost_equal(rgorg_album_gain, rva2_album_gain)):
             # The different formats are not similar enough.
             return (None, None)
         else:
             # the formats seem to match, we obviously use the non-clamped one
-            return rgorg
+            return (rgorg_track_gain, rgorg_album_gain)
 
     def write_gain(self, filename, track_gain, album_gain):
         self.rgorg.write_gain(filename, track_gain, album_gain)
@@ -293,15 +303,18 @@ GAIN_EPSILON = 0.1
 PEAK_EPSILON = 0.001
 REF_LEVEL_EPSILON = 0.1
 
-# For these three functions, b is always the legacy values, i.e. the potentially
-# clamped ones.
+
+# For these three functions, b is always the legacy values, i.e. the
+# potentially clamped ones.
 def gain_almost_equal(a, b):
     return (almost_equal(a, b, GAIN_EPSILON) or
-        almost_equal(clamp_rva2_gain(a), b, GAIN_EPSILON))
+            almost_equal(clamp_rva2_gain(a), b, GAIN_EPSILON))
+
 
 def peak_almost_equal(a, b):
     return (almost_equal(a, b, PEAK_EPSILON) or
-        almost_equal(clamp_rva2_peak(a), b, PEAK_EPSILON))
+            almost_equal(clamp_rva2_peak(a), b, PEAK_EPSILON))
+
 
 def gaindata_almost_equal(a, b):
     # Ensure neither element is None.
@@ -315,13 +328,14 @@ def gaindata_almost_equal(a, b):
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore")
         return (gain_almost_equal(a.gain, b.gain) and
-            peak_almost_equal(a.peak, b.peak) and
-            almost_equal(a.ref_level, b.ref_level, REF_LEVEL_EPSILON))
+                peak_almost_equal(a.peak, b.peak) and
+                almost_equal(a.ref_level, b.ref_level, REF_LEVEL_EPSILON))
 
 
 # code to pull everything together
 class UnknownFiletype(Exception):
     pass
+
 
 class BaseFormatsMap(object):
     _simplereaderwriter = SimpleTagReaderWriter()
@@ -331,7 +345,7 @@ class BaseFormatsMap(object):
     _mp3_default_readerwriter = MP3DefaultTagReaderWriter(
         _mp3_rgorg_readerwriter,
         _mp3_rva2_readerwriter)
-    
+
     BASE_MAP = {
         ".ogg": _simplereaderwriter,
         ".oga": _simplereaderwriter,
@@ -352,7 +366,7 @@ class BaseFormatsMap(object):
 
     MP3_DISPLAY_FORMATS = ["default", "replaygain.org", "legacy", "ql", "fb2k"]
     MP3_DEFAULT_FORMAT = "default"
-    
+
     def __init__(self, mp3_format=None, more_mappings=None):
         # yeah, you need to choose
         self.more_mappings = more_mappings if more_mappings else {}
@@ -364,7 +378,7 @@ class BaseFormatsMap(object):
     def is_supported_format(self, ext):
         ext_lower = ext.lower()
         return ext_lower in self.BASE_MAP or ext_lower in self.more_mappings
-    
+
     def read_gain(self, filename):
         ext = os.path.splitext(filename)[1].lower()
         if ext in self.more_mappings:
@@ -373,9 +387,9 @@ class BaseFormatsMap(object):
             accessor = self.BASE_MAP[ext]
         else:
             raise UnknownFiletype(ext)
-        
+
         return accessor.read_gain(filename)
-    
+
     def write_gain(self, filename, trackgain, albumgain):
         ext = os.path.splitext(filename)[1].lower()
         if ext in self.more_mappings:
