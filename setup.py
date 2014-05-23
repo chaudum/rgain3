281a180cf5b1aa2a3053024525605063ea7b72c0
Felix Krull <f_krull@gmx.de>
2014-05-23 17:06:17 +0200

    Clean setup.py formatting.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from contextlib import closing
+from __future__ import print_function
+
 import os
 import sys
 from distutils.core import Command, Distribution, setup
 from distutils.command.build import build
-from distutils.errors import DistutilsOptionError
 
 try:
     import docutils.core
@@ -24,7 +24,7 @@ try:
         def initialize_options(self):
             self.rst_manpages = None
             self.outputdir = None
-        
+
         def finalize_options(self):
             if not self.outputdir:
                 self.outputdir = os.path.join("build", "man")
@@ -34,13 +34,14 @@ try:
             if not self.rst_manpages:
                 return
             if not os.path.exists(self.outputdir):
-                os.makedirs(self.outputdir, mode=0755)
+                os.makedirs(self.outputdir, mode=0o755)
             for infile, outfile in self.rst_manpages:
-                print "Converting %s to %s ..." % (infile, outfile),
-                docutils.core.publish_file(source_path=infile,
-                        destination_path=os.path.join(self.outputdir, outfile),
-                        writer_name="manpage")
-                print "ok"
+                print("Converting %s to %s ..." % (infile, outfile), end="")
+                docutils.core.publish_file(
+                    source_path=infile,
+                    destination_path=os.path.join(self.outputdir, outfile),
+                    writer_name="manpage")
+                print("ok")
 
     build.sub_commands.append(("build_manpages", None))
     manpages_args = {
@@ -77,12 +78,13 @@ setup(
     long_description="""\
 A set of Python modules and utility programmes to deal with Replay Gain
 information -- calculate it (with GStreamer), read and write it (with Mutagen).
-It has support for Ogg Vorbis (or probably anything stored in an Ogg container),
-Flac, WavPack, MP4 (aka AAC) and MP3 (in different incarnations). There‘s also
-a command-line programme, ``replaygain``, that works very similar to its like-
-named cousins, most prominently ``vorbisgain`` and ``mp3gain`` -- only that it
-works for all those supported formats alike. ``collectiongain`` on the other
-hand is a kind of fire-and-forget tool for big amounts of music files.
+It has support for Ogg Vorbis (or probably anything stored in an Ogg
+container), Flac, WavPack, MP4 (aka AAC) and MP3 (in different incarnations).
+There‘s also a command-line programme, ``replaygain``, that works very similar
+to its like-named cousins, most prominently ``vorbisgain`` and ``mp3gain`` --
+only that itworks for all those supported formats alike. ``collectiongain``
+on the other hand is a kind of fire-and-forget tool for big amounts of music
+files.
 """,
 
     packages=["rgain", "rgain.script"],
