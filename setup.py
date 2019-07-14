d03feba6179221ae84918c5275e2b7e074065759
Christian Haudum <christian.haudum@gmail.com>
2019-07-14 21:50:48 +0200

    Rename package to rgain2
    * Update package name
    * Update author information in setup.py
    * Increase upcoming version to 2.0.dev0

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/env/bin python3
 
+"""
+A set of Python modules and utility programmes to deal with Replay Gain
+information -- calculate it (with GStreamer), read and write it (with Mutagen).
+It has support for Ogg Vorbis (or probably anything stored in an Ogg
+container), Flac, WavPack, MP4 (aka AAC) and MP3 (in different incarnations).
+There‘s also a command-line programme, ``replaygain``, that works very similar
+to its like-named cousins, most prominently ``vorbisgain`` and ``mp3gain`` --
+only that itworks for all those supported formats alike. ``collectiongain`` on
+the other hand is a kind of fire-and-forget tool for big amounts of music
+files.
+"""
 
 import os
 import sys
@@ -103,19 +113,19 @@ except ImportError:
     manpages_args = {}
 
 setup(
-    name="rgain",
+    name="rgain2",
     version=__version__,
-    description="Multi-format Replay Gain utilities",
-    author="Felix Krull",
-    author_email="f_krull@gmx.de",
-    url="http://bitbucket.org/fk/rgain",
-    license="GNU General Public License (v2 or later)",
+    description="Multi-format Replay Gain utilities for Python 3",
+    author="Christian Haudum",
+    author_email="christian@christianhaudum.at",
+    url="https://github.com/chaudum/rgain/",
+    license="GNU General Public License v2 or later",
     classifiers=[
-        "Development Status :: 7 - Inactive",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: End Users/Desktop",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)"
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
@@ -123,26 +133,11 @@ setup(
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    long_description="""\
-A set of Python modules and utility programmes to deal with Replay Gain
-information -- calculate it (with GStreamer), read and write it (with Mutagen).
-It has support for Ogg Vorbis (or probably anything stored in an Ogg
-container), Flac, WavPack, MP4 (aka AAC) and MP3 (in different incarnations).
-There‘s also a command-line programme, ``replaygain``, that works very similar
-to its like-named cousins, most prominently ``vorbisgain`` and ``mp3gain`` --
-only that itworks for all those supported formats alike. ``collectiongain``
-on the other hand is a kind of fire-and-forget tool for big amounts of music
-files.
-
-NOTE: rgain is currently not being developed; for more information or if you'd
-like to help remedying this situation, see:
-https://bitbucket.org/fk/rgain/issues/26/wanted-new-maintainer
-""",
-
+    long_description=__doc__,
+    long_description_content_type="text/x-rst",
     packages=["rgain", "rgain.script"],
     scripts=["scripts/replaygain", "scripts/collectiongain"],
     install_requires=["pygobject", "mutagen"],
     python_requires=">=3.5",
-
     **manpages_args
 )
