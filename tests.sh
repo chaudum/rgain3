40054476025612e0d1756b19c1a577c584ce7e22
Christian Haudum <christian.haudum@gmail.com>
2019-07-18 08:45:25 +0200

    Clean up rgain.egg-info/ directory after test runs

--- tests.sh
+++ tests.sh
@@ -3,4 +3,4 @@
 pip install -e .
 pytest -vvv -s --cache-clear
 find . -name __pycache__ -type d | xargs rm -rf
-rm -rf .pytest_cache/ .mypy_cache/
+rm -rf .pytest_cache/ .mypy_cache/ rgain.egg-info/
