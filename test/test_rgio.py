15044766d1c6199f9a61bf85c10a8248a919c2f2
Christian Haudum <christian@christianhaudum.at>
2019-11-09 23:00:31 +0100

    Create empty ID3 tags object if missing
    This fixes a bug that caused rgain to fail in case the provided file
    did not have ID3 headers.

---
+++ test/test_rgio.py
@@ -0,0 +1,33 @@
+import os.path
+import unittest
+from pathlib import Path
+
+from rgain.rgio import MP3TagReaderWriter
+
+DATA_PATH = Path(os.path.dirname(__file__)) / "data"
+
+
+class TestGetTagsObject(unittest.TestCase):
+    def setUp(self):
+        self.reader_writer = MP3TagReaderWriter()
+
+    def test_no_headers_mp3(self):
+        tags = self.reader_writer._get_tags_object(
+            str(DATA_PATH / "missing-headers.mp3")
+        )
+        self.assertEquals(tags.keys(), [])
+        self.assertTrue(tags.filename.endswith("missing-headers.mp3"))
+
+    def test_no_tags_mp3(self):
+        tags = self.reader_writer._get_tags_object(
+            str(DATA_PATH / "no-tags.mp3")
+        )
+        self.assertEquals(tags.keys(), [])
+        self.assertTrue(tags.filename.endswith("no-tags.mp3"))
+
+    def test_album_mp3(self):
+        tags = self.reader_writer._get_tags_object(
+            str(DATA_PATH / "album-tag.mp3")
+        )
+        self.assertEquals(tags.keys(), ["album"])
+        self.assertTrue(tags.filename.endswith("album-tag.mp3"))
