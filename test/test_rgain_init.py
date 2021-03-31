870927421fadc460c3823a63048e63549ba045e5
Christian Haudum <christian.haudum@gmail.com>
2021-03-31 22:50:40 +0200

    Fix syntax error in string formatting of GainData class

--- test/test_rgain_init.py
+++ test/test_rgain_init.py
@@ -4,6 +4,26 @@ from rgain3.lib import GainData, GainType
 
 
 class TestGainData(unittest.TestCase):
+    def test_str(self):
+        self.assertEqual(
+            str(GainData(-1.2)),
+            "gain=-1.20 dB; peak=1.00000000; reference-level=89 dB"
+        )
+        self.assertEqual(
+            str(GainData(-1.2, 0.6, 88, GainType.TP_ALBUM)),
+            "gain=-1.20 dB; peak=0.60000000; reference-level=88 dB"
+        )
+
+    def test_repr(self):
+        self.assertEqual(
+            repr(GainData(-1.2)),
+            "GainData(-1.2, 1.0, 89, GainType.TP_UNDEFINED)",
+        )
+        self.assertEqual(
+            repr(GainData(-1.2, 0.6, 88, GainType.TP_ALBUM)),
+            "GainData(-1.2, 0.6, 88, GainType.TP_ALBUM)",
+        )
+
     def test_eq(self):
         gd1 = GainData(-5, 0.5, 80)
         gd2 = GainData(-5, 0.5, 80)
